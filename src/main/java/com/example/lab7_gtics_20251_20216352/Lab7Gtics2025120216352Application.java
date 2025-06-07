a0815f250f63d043e8fc4dc838271b2f74792cd3
OhMondayMonday <a20216352@pucp.edu.pe>
2025-06-06 20:05:40 -0500

    Initial commit

---
+++ src/main/java/com/example/lab7_gtics_20251_20216352/Lab7Gtics2025120216352Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_gtics_20251_20216352;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7Gtics2025120216352Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab7Gtics2025120216352Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab7_gtics_20251_20216352/Lab7Gtics2025120216352ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_gtics_20251_20216352;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7Gtics2025120216352ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
